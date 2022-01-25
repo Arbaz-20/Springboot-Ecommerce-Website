04e6932983c4292a97b049531f029f6e595adc7f
Arbaz Khan <khanarbaz26393@gmail.com>
2022-01-25 20:59:11 +0530

    project-changes

--- src/main/java/com/ecommerce/ecommercewebsite/EcommerceWebsiteApplication.java
+++ src/main/java/com/ecommerce/ecommercewebsite/EcommerceWebsiteApplication.java
@@ -2,12 +2,8 @@ package com.ecommerce.ecommercewebsite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-
-import Controller.MainController;
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses = MainController.class)
 public class EcommerceWebsiteApplication {
 
 	public static void main(String[] args) {
